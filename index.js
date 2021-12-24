493b4c5b57b6bd0864fbbeef4f7c4bb4708fcd89
Fapret <78763775+fapret@users.noreply.github.com>
2021-12-24 05:49:25 -0300

    code simplified

--- index.js
+++ index.js
@@ -40,9 +40,10 @@ const menu = async function(){
     }
 }
 
-const DecryptMenu = async function(){
-    var isInValidOption = true;
+//simple selectAlgorithm menu
+const selectAlgorithm = async function(){
     var cancel = false;
+    var isInValidOption = true;
     while (isInValidOption){
         var answer = await new Promise(answer => {
             rl.question('Which encryption algorithm you want to use?\n 1. aes-256-ctr (default)\n 2. aes-256-cbc\n 3. aes-256-cfb\n 4. aes-256-ofb\n 5. Cancel\n', (answer));
@@ -74,7 +75,13 @@ const DecryptMenu = async function(){
                 break;
         }
     }
-    if(!cancel){
+    return cancel;
+}
+
+//Decrypt Menu with decryption code over crypto library
+const DecryptMenu = async function(){
+    var algorithmSelected = await selectAlgorithm();
+    if(!algorithmSelected){
         var answer = await new Promise(answer => {
             rl.question('Please write a password:\n', (answer));
         });
@@ -86,91 +93,58 @@ const DecryptMenu = async function(){
             } else {
                 fileToDecrypt = fs.readFileSync('./in/' + file);
                 fileToDecrypt = fileToDecrypt.toString();
-                //EncryptFile(fileToEncrypt, './in/' + file, file, secretKey, iv);
                 let fileParts = fileToDecrypt.split(':');
                 let iv = Buffer.from(fileParts.shift(), 'hex');
                 let encryptedFile = Buffer.from(fileParts.join(':'), 'hex');
                 let decipher = crypto.createDecipheriv(currentAlgorithm, answer, iv);
                 let decrypted = decipher.update(encryptedFile);
                 decrypted = Buffer.concat([decrypted, decipher.final()]);
-                fs.writeFileSync('./out/' + file, decrypted);
-                console.log('file Decrypted: ' + './out/' + file);
+                if(fs.existsSync('./out/' + file)){
+                    console.log('Ya existe archivo con nombre ' + file + ' en ./out por lo tanto no se pudo desencriptar ./in/' + file);
+                } else {
+                    try{
+                        fs.writeFileSync('./out/' + file, decrypted);
+                        console.log('file Decrypted: ' + './out/' + file);
+                    } catch (err) {
+                        console.log('unexpected error:\n' + err);
+                    }
+                }
             }
         });
     }
 }
 
+//Encryption Menu with encryption code over crypto library
 const EncryptMenu = async function(){
-    var isInValidOption = true;
-    var cancel = false;
-    while (isInValidOption){
-        var answer = await new Promise(answer => {
-            rl.question('Which encryption algorithm you want to use?\n 1. aes-256-ctr (default)\n 2. aes-256-cbc\n 3. aes-256-cfb\n 4. aes-256-ofb\n 5. Cancel\n', (answer));
-        });
-        switch (answer){
-            case '1':
-                currentAlgorithm = 'aes-256-ctr';
-                isInValidOption = false;
-                break;
-            case '2':
-                currentAlgorithm = 'aes-256-cbc';
-                isInValidOption = false;
-                break;
-            case '3':
-                currentAlgorithm = 'aes-256-cfb';
-                isInValidOption = false;
-                break;
-            case '4':
-                currentAlgorithm = 'aes-256-ofb';
-                isInValidOption = false;
-                break;
-            case '5':
-                isInValidOption = false;
-                cancel = true;
-                break;
-            default:
-                console.log(answer + ' is not a valid option');
-                isInValidOption = true;
-                break;
-        }
-    }
-    if(!cancel){
+    var algorithmSelected = await selectAlgorithm();
+    if(!algorithmSelected){
         answer = await new Promise(answer => {
             rl.question('Please write a password:\n', (answer));
         });
         var iv = crypto.randomBytes(16);
         answer = crypto.createHash('sha256').update(String(answer)).digest();
-        await encryptAll(answer, iv);
+        files = fs.readdirSync('./in');
+        files.forEach(function(file) {
+            if (fs.statSync('./in/' + file).isDirectory()){
+                console.log('./in/' + file + ' is a directory, it will be ignored...');
+            } else {
+                fileToEncrypt = fs.readFileSync('./in/' + file);
+                const cipher = crypto.createCipheriv(currentAlgorithm, secretKey, iv);
+                let endmsg = cipher.update(fileToEncrypt);
+                endmsg = Buffer.concat([endmsg, cipher.final()]);
+                if(fs.existsSync('./out/' + file)){
+                    console.log('Ya existe archivo con nombre ' + file + ' en ./out por lo tanto no se pudo encriptar ./in/' + file);
+                } else {
+                    try{
+                        fs.writeFileSync('./out/' + file, iv.toString('hex') + ':' + endmsg.toString('hex'));
+                        console.log('file encrypted: ./in/' + file);
+                    } catch (err) {
+                        console.log('unexpected error:\n' + err);
+                    }
+                }
+            }
+        });
     }
-}
-
-const EncryptFile = async function(file, path, filename, secretKey, iv){
-    const cipher = crypto.createCipheriv(currentAlgorithm, secretKey, iv);
-    let endmsg = cipher.update(file);
-    endmsg = Buffer.concat([endmsg, cipher.final()]);
-    if(fs.existsSync('./out/' + filename)){
-        console.log('Ya existe archivo con nombre ' + filename + ' en ./out por lo tanto no se pudo encriptar ' + path);
-    } else {
-        try{
-            //fs.writeFileSync('./out/' + filename, endmsg);
-            fs.writeFileSync('./out/' + filename, iv.toString('hex') + ':' + endmsg.toString('hex'));
-            console.log('file encrypted: ' + path);
-        } catch (err) {
-            console.log('unexpected error:\n' + err);
-        }
-    }
-}
-
-const encryptAll = async function(secretKey, iv){
-    files = fs.readdirSync('./in');
-    files.forEach(function(file) {
-        if (fs.statSync('./in/' + file).isDirectory()){
-            console.log('./in/' + file + ' is a directory, it will be ignored...');
-        } else {
-            fileToEncrypt = fs.readFileSync('./in/' + file);
-            EncryptFile(fileToEncrypt, './in/' + file, file, secretKey, iv);
-        }
-    });
 }
 
 menu();
